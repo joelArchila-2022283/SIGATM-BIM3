b98712a63ce6a1f6d6ca244b667139d840b206c5
joelArchila-2022283 <jarchila-2022283@kinal.edu.gt>
2026-07-13 20:59:15 -0600

    feat: Se implemento el arreglo de usuario en data

---
+++ sigatm-backend/src/data/usuariosData.ts
@@ -0,0 +1,13 @@
+import { Usuario } from '../models/usuario';
+
+export const usuariosSimulados: Usuario[] = [
+    {
+        id: 1,
+        nombre: "Joel Francisco",
+        apellido: "Archila Dávila",
+        correo: "joel.archila@kinal.edu.gt",
+        username: "jarchila",
+        password: "password123",
+        rolId: 1 // Administrador
+    }
+];
