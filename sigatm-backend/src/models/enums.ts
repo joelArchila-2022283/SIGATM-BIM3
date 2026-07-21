a3e516bc2f845fcd0bc959dbfc6048f46cdfbc1f
joelArchila-2022283 <jarchila-2022283@kinal.edu.gt>
2026-07-20 20:33:24 -0600

    feat: Se agregaron los enums para poder utilizarlos en los models

---
+++ sigatm-backend/src/models/enums.ts
@@ -0,0 +1,16 @@
+export enum EstadoEquipo {
+    ACTIVO = 'activo',
+    INACTIVO = 'inactivo',
+    EN_MANTENIMIENTO = 'en_mantenimiento'
+}
+
+export enum TipoMantenimiento {
+    PREVENTIVO = 'preventivo',
+    CORRECTIVO = 'correctivo'
+}
+
+export enum EstadoMantenimiento {
+    PENDIENTE = 'pendiente',
+    EN_PROGRESO = 'en_progreso',
+    FINALIZADO = 'finalizado'
+}
